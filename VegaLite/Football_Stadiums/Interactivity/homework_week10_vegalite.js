e7df05840a49c422faf22eae3eba825d98e5098c
skai0008 <kaingsteven123@gmail.com>
2025-10-10 00:17:05 +1100

    feat: add stadiums per capita html

---
+++ VegaLite/Football_Stadiums/Interactivity/homework_week10_vegalite.js
@@ -0,0 +1,15 @@
+
+var vg_1 = "top_100_stadiums.vg.json";
+var vg_2 = "stadiums_per_capita.vg.json";
+
+vegaEmbed("#geo_map", vg_1).then(function(result) {
+    
+    result.view;
+
+}).catch(console.error);
+
+vegaEmbed("#stadiums_per_capita", vg_2).then(function(result) {
+    
+    result.view;
+
+}).catch(console.error);
