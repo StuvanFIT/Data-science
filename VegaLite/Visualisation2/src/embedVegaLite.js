cb8480719a7385c6ae8dc616edcadbafe8fba370
skai0008 <kaingsteven123@gmail.com>
2025-10-17 02:58:34 +1100

    feat: add comparison section

--- VegaLite/Visualisation2/src/embedVegaLite.js
+++ VegaLite/Visualisation2/src/embedVegaLite.js
@@ -12,7 +12,7 @@ vegaEmbed("#geo_map", vg_1).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#stadiums_per_capita", vg_2).then(function(result) {
-    
+
     result.view;
 
 }).catch(console.error);
